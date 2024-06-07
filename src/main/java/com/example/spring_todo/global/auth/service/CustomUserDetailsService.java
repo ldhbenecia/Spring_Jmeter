5e2795406e10460618b41d5202c501a0c02436e5
DongHyeok Lim <jh07050@gmail.com>
2024-06-07 22:46:42 +0900

    feat: 사용자 인증정보 획득 로직

---
+++ src/main/java/com/example/spring_todo/global/auth/service/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.example.spring_todo.global.auth.service;
+
+import com.example.spring_todo.domain.user.domain.User;
+import com.example.spring_todo.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+
+        return new PrincipleDetails(user);
+    }
+}
---
+++ src/main/java/com/example/spring_todo/global/auth/service/PrincipleDetails.java
@@ -0,0 +1,30 @@
+package com.example.spring_todo.global.auth.service;
+
+import com.example.spring_todo.domain.user.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class PrincipleDetails implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(() -> "ROLE_USER");
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+}
