a1eee74793c07c4698eefb05ff0b5a98f406de98
DongHyeok Lim <jh07050@gmail.com>
2024-06-08 00:01:50 +0900

    feat: 회원가입 validation 기능 추가

--- src/main/java/com/example/spring_todo/domain/user/controller/UserController.java
+++ src/main/java/com/example/spring_todo/domain/user/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.spring_todo.domain.user.controller;
 import com.example.spring_todo.domain.user.dto.UserSignUpRequestDto;
 import com.example.spring_todo.domain.user.dto.UserSignUpResponseDto;
 import com.example.spring_todo.domain.user.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +17,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/api/signup")
-    public ResponseEntity<UserSignUpResponseDto> signUp(@RequestBody UserSignUpRequestDto requestDto) {
+    public ResponseEntity<UserSignUpResponseDto> signUp(@RequestBody @Valid UserSignUpRequestDto requestDto) {
         UserSignUpResponseDto createUser = userService.createUser(requestDto);
         return ResponseEntity.ok(createUser);
     }
--- src/main/java/com/example/spring_todo/domain/user/dto/UserSignUpRequestDto.java
+++ src/main/java/com/example/spring_todo/domain/user/dto/UserSignUpRequestDto.java
@@ -1,12 +1,21 @@
 package com.example.spring_todo.domain.user.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class UserSignUpRequestDto {
+
+    @Email
+    @NotBlank
     private String email;
+
+    @NotBlank
     private String nickname;
+
+    @NotBlank
     private String password;
 }
