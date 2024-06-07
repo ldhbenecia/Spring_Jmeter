b9ce27818c818a20c6a2002c0b514503789c9464
DongHyeok Lim <jh07050@gmail.com>
2024-06-07 23:17:48 +0900

    feat: User 테이블 엔티티 설계

---
+++ src/main/java/com/example/spring_todo/domain/user/domain/User.java
@@ -0,0 +1,32 @@
+package com.example.spring_todo.domain.user.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String password;
+
+    public User(String email, String nickname, String password) {
+        this.email = email;
+        this.nickname = nickname;
+        this.password = password;
+    }
+}
