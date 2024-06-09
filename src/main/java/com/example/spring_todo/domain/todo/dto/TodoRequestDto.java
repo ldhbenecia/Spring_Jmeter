40ccde75c84e469c4f4e728bf95b2153048a1b01
DongHyeok Lim <jh07050@gmail.com>
2024-06-09 20:29:21 +0900

    feat: TodoRequestDto 유효성 추가

--- src/main/java/com/example/spring_todo/domain/todo/dto/TodoRequestDto.java
+++ src/main/java/com/example/spring_todo/domain/todo/dto/TodoRequestDto.java
@@ -1,10 +1,13 @@
 package com.example.spring_todo.domain.todo.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class TodoRequestDto {
+
+    @NotBlank
     private String contents;
 }
